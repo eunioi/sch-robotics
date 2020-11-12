b2f4a9dabd6e8eb433e9f06e6c072c4f22735d5b
eun <kgm5004@sch.ac.kr>
2020-11-12 14:15:43 +0900

    change main to use list ops

--- git_practice/main.py
+++ git_practice/main.py
@@ -4,18 +4,22 @@
 
 ## enfwnddp anjfh wjwkdgkfrjsi head == loacl gkrrydptj tnwjdgksrj 
 ## ==>> dkfoqnqnsdms wlqdptj tnwjdgks qnqns
-<<<<<<< HEAD
+#<<<<<<< HEAD
 ## rltgjqmdp dhffuTejs sodyd
 # 수정 예시
 #if __name__ == "__main__":
 #    print("hello python, hi ros.")
 #    print("Python is the best language for ros")
-=======
+#=======
 #다음엔 기존에 있던 anything.py를 변경한다. (gedit anything.py)
 
-from add_lists import add_lists
+# main.py
+import list_ops as lo
 
 if __name__ == "__main__":
-    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
-    print(result)
->>>>>>> 1caaed8ae2d6cd0a82d107b7163f4b837a65781e
+    foo = [1,2,3]
+    bar = [4,5,6]
+    print("foo+bar=", lo.add(foo, bar))
+    print("foo-bar=", lo.subtract(foo, bar))
+    print("foo*bar=", lo.multiply(foo, bar))
+    print("foo/bar=", lo.divide(foo, bar))
