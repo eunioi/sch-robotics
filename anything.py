23c0d356fa234708a3986aa0789731801dea7fac
eun <kgm5004@sch.ac.kr>
2020-11-12 13:43:15 +0900

    add function add_lists

--- anything.py
+++ anything.py
@@ -1,3 +1,11 @@
 # 내용 작성 예시
+#if __name__ == "__main__":
+#    print("hello python, hi ros.")
+
+#다음엔 기존에 있던 anything.py를 변경한다. (gedit anything.py)
+
+from add_lists import add_lists
+
 if __name__ == "__main__":
-    print("hello python, hi ros.")
+    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
+    print(result)
